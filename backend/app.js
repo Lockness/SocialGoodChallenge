6aee9ceb52b9a7be1eab3f98c24fd70ae2657090
Justin Carruthers <jtcruthers@gmail.com>
2017-10-21 11:55:30 -0400

    Finished init commit backend.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
+var indexRoutes = require('./routes/index');
 var port = 3001;
 
 // uncomment after placing your favicon in /public
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
