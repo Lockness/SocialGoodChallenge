e885cd1e0fbf17910fbb4b5f50d7e8ce0b9ad760
Ryan Tomlinson <ryantomlinson@MacBook-Pro.local>
2017-10-21 18:51:28 -0400

    jumbotron

--- frontend/src/components/AppComponent.js
+++ frontend/src/components/AppComponent.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import Jumbo from './Jumbotron'
 
 class AppComponent extends Component {
 
   componentWillReceiveProps(props) {
     console.log(this.props.testt);
   }
-  
+
   render() {
+
     return (
+
       <div className="App">
         <div className="App-header">
           <Navbar />
-          <h1>Welcome to SocialServe</h1>
+          <Jumbo />
         </div>
       </div>
     );
---
+++ frontend/src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
+import pic from './jumbotronPic.jpg';
+
+const Jumbo = () => (
+
+  <Jumbotron>
+    <img src={pic} style={{height:"500px", width:"100%"}} />
+    <p> Social Serve </p>
+  </Jumbotron>
+)
+
+export default Jumbo;
