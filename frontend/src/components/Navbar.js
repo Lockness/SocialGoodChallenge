1d58d0cafa27923e1faca1f2b08971f52ea8e500
Justin Carruthers <jtcruthers@gmail.com>
2017-10-21 17:09:41 -0400

    Fixed navbar.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
-import {Nav} from 'react-bootstrap';
-import {NavItem} from 'react-bootstrap';
-import {DropdownButton} from 'react-bootstrap';
-import {MenuItem} from 'react-bootstrap';
-import {NavDropdown} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, DropdownButton, MenuItem, NavDropdown} from 'react-bootstrap';
 
 const Navbarr = () => (
   <Navbar inverse collapseOnSelect>
